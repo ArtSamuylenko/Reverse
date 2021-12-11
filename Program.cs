042754d5176e3004145e2009a0c276b43f764a81
ArtSamuylenko <89385274775@mail.ru>
2021-12-12 02:07:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str_2 = new string(str.ToCharArray().Reverse().ToArray());
+            Console.WriteLine(str_2);
+            Console.ReadLine();
+        }
+    }
+}
